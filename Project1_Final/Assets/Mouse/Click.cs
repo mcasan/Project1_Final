4484fb1e4ec6e6c983875f28d039aeac2fe24209
mcasan <31909324+mcasan@users.noreply.github.com>
2017-10-18 18:31:09 -0400

    Add empty project

---
+++ Project1_Final/Assets/Mouse/Click.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+	
+	public string Medal_Type;
+
+	void OnMouseDown(){
+
+		Destroy (gameObject); 
+
+
+		
+					  }
+
+
+}
---
+++ Project1_Final/Assets/Points.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour {
+ 
+public	int bronzePoints = 1;
+public	int silverPoints = 10;
+public	int goldPoints = 100;
+public  int totalScore;
+ 
+	// Use this for initialization
+	void Start () {
+		
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
