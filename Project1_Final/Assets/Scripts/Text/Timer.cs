754dc4f191a4b196ce76b51b7ece19d8043b3e1e
mcasan <31909324+mcasan@users.noreply.github.com>
2017-11-27 17:30:38 -0500

    add it
    this is the timer

---
+++ Project1_Final/Assets/Scripts/Text/Timer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Timer : MonoBehaviour {
+
+	public Text gameTimerText;
+
+	float gameTimer = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+
+		gameTimer += Time.deltaTime;
+
+		int seconds = (int)(gameTimer % 60);
+
+		int minutes = (int)(gameTimer / 60) % 60;
+
+		int hours = (int)(gameTimer / 3600) % 24;
+
+		string timerString = string.Format("{0:0}:{1:00}:{2:00}", hours, minutes, seconds);
+
+		gameTimerText.text = timerString;
+	}
+}
